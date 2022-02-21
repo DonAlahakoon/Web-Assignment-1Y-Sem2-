a5700fa780cad61a000ee1df0c354e87fc8a0929
Praveen <praveent8@gmail.com>
2022-02-21 22:33:11 +0530

    Updates

--- js/gcc.js
+++ js/gcc.js
@@ -9,3 +9,20 @@ function carousel() {
   x[myIndex-1].style.display = "block";
   setTimeout(carousel, 5000); // Change image every 2 seconds
 }
+
+function readmore(){
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("btn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+
+}
